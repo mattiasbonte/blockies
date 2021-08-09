b0563838823c4ba69ee355abdf53e7693bc07ccd
Mattias Bonte <mb@guideline.be>
2021-08-09 19:50:32 +0200

    Update Border Styling

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Container = (props: { children: any }) => {
     return (
         <div className="dark:bg-[#2f3437] bg-[#ffffff] dark:text-emerald-400 text-violet-100 h-screen">
-            <div className="max-w-7xl lg:px-8 p-1 mx-auto">{props.children}</div>
+            <div className="max-w-7xl mx-auto">{props.children}</div>
         </div>
     )
 }
